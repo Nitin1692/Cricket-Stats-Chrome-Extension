134226b5f52d3b97f660f300eef828cf1a3c603f
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2023-01-01 15:43:28 +0530

    Cricket stats chrome extension

---
+++ display.js
@@ -0,0 +1,23 @@
+const tableData= document.querySelector(".table-body")
+async function fetchFromOffset(offset) {
+    return await fetch('https://api.cricapi.com/v1/matches?apikey=5259b967-c3f8-469a-91e8-464cc450d922&offset=' + offset)
+        .then(data => data.json())
+        .then(data => {
+            let output =""
+            data.data.map(stats => {
+                output += `
+            <tr>
+                <td>${stats.name}</td>
+                <td>${stats.matchType}</td>
+                <td>${stats.status}</td>
+                <td>${stats.venue}</td>
+                <td>${stats.date}</td>
+              </tr>
+                `
+            })
+            tableData.innerHTML = output
+        })
+        .catch(e => console.log(e));
+}
+
+fetchFromOffset(0);
